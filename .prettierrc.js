9ed4dcfdfe1cbd3964c7e06c9dae3b230f271369
Wangjunjie991116 <2638241171@qq.com>
2023-10-15 17:43:30 +0800

    feat: config init

---
+++ .prettierrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    // Indent with tabs instead of spaces.
+    useTabs: false,
+    // Number of spaces per indentation level.
+    tabWidth: 4,
+    // Print semicolons.  semi: true,
+    // The line length where Prettier will try wrap.
+    printWidth: 120,
+    // Use single quotes instead of double quotes.
+    singleQuote: true,
+    // Include parentheses around a sole arrow function parameter.
+    arrowParens: 'avoid',
+    // Print spaces between brackets.
+    bracketSpacing: true,
+    // Print trailing commas wherever possible when multi-line.
+    trailingComma: 'es5',
+    // Put > on the new line instead of at last line.
+    jsxBracketSameLine: false,
+    // Use single quotes in JSX.
+    jsxSingleQuote: false,
+    // How to handle whitespaces in HTML.
+    htmlWhitespaceSensitivity: 'css',
+    // How to wrap prose.
+    proseWrap: 'always',
+};
