2997e5fe454b22db0e739957d01c18c1badb428a
Wangjunjie991116 <2638241171@qq.com>
2024-05-27 22:05:33 +0800

    fix: npm发包脚本的循环发包问题解决

--- npm-publish.sh
+++ npm-publish.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-# 使用快捷发包命令, 需要提前赋予脚本文件执行权限, chmod +x npm-publish.sh
+# 发布 npm
+# 使用快捷发包命令, 若出现权限问题, 执行 chmod +x npm-publish.sh 赋予脚本文件执行权限
 # pnpm publish major     # 发布 major 版本
 # pnpm publish minor     # 发布 minor 版本
 # pnpm publish patch     # 发布 patch 版本
 # pnpm publish           # 默认 patch 版本
 if [ -z "$1" ]; then
-  npm version patch
+  VERSION="patch"
 elif [ "$1" == "major" ]; then
-  npm version major
+  VERSION="major"
 elif [ "$1" == "minor" ]; then
-  npm version minor
+  VERSION="minor"
 elif [ "$1" == "patch" ]; then
-  npm version patch
+  VERSION="patch"
 else
   echo "Invalid argument. Usage: publish.sh [major|minor|patch]"
   exit 1
 fi
+npm version $VERSION
 npm publish
 echo "******************************** 牛啊小老弟儿, npm 包完成发版 ******************************** "
 
-# 推送代码到 github
+# 推送 github
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 REMOTE_BRANCH=$(git ls-remote --heads origin $BRANCH)
 if [ -z "$REMOTE_BRANCH" ]; then
